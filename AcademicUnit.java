ad87dc1744e0eb0800c8f2ee2f07518be5ca96fe
Wunna <wannamoesan11@gmail.com>
2025-11-14 02:46:58 +0700

    Added Interface and Implementation

---
+++ AcademicUnit.java
@@ -0,0 +1,12 @@
+package Java;
+
+public interface AcademicUnit {
+
+    // Get the name of the current Unit
+    public String getFacultyName();
+
+    // Describe the current Unit
+    public void describe();
+}
+
+
---
+++ FacultyDemo.java
@@ -0,0 +1,32 @@
+package Java;
+
+public class FacultyDemo {
+    public static void main(String[] args) {
+
+        // Creating objects based on Faculty class
+        Faculty sit = new Faculty("School of Information Technology", "Prof. Narongrit Waraporn");
+        Faculty engineering = new Faculty("Faculty of Engineering", "Prof. Tawich Pulngern");
+        Faculty liberalArts = new Faculty("School of Liberal Arts", "Prof. Natjiree Jaturapitakkul");
+        Faculty archi = new Faculty("School of Architecture and Design", "Prof. Waraluk Pansuwan");
+
+        // Display SIT
+        System.out.println("SIT information");
+        sit.describe();
+        System.out.println();
+
+        System.out.println("Technical Faculties");
+        // Method Overloading
+        // Provide Technical Faculties 
+        sit.displayFacultyType(true);
+        System.out.println();
+        // Provide Technical Faculties along with programs it offers
+        sit.displayFacultyType(true, 3);
+        System.out.println();
+
+
+        System.out.println("Non-Technical Faculties");
+        archi.displayFacultyType(false);
+        System.out.println();
+        archi.displayFacultyType(false, 1);
+    }
+}
